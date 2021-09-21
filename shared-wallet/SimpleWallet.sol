c44586a81d928c4755cf35be9474a20e7f47f4e3
Rafael Flores <fita500@gmail.com>
2021-09-21 11:59:37 -0600

    Add events for allowance

--- shared-wallet/SimpleWallet.sol
+++ shared-wallet/SimpleWallet.sol
@@ -3,9 +3,17 @@ pragma solidity ^0.5.13;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
 contract Allowance is Ownable {
+    event AllowanceChanged(
+        address indexed _forWho,
+        address indexed _fromWhom,
+        uint256 _oldAmount,
+        uint256 _newAmount
+    );
+
     mapping(address => uint256) public allowance;
 
     function addAllowance(address _who, uint256 _amount) public onlyOwner {
+        emit AllowanceChanged(_who, msg.sender, allowance[_who], _amount);
         allowance[_who] = _amount;
     }
 
@@ -18,6 +26,12 @@ contract Allowance is Ownable {
     }
 
     function reduceAllowance(address _who, uint256 _amount) internal {
+        emit AllowanceChanged(
+            _who,
+            msg.sender,
+            allowance[_who],
+            allowance[_who] - _amount
+        );
         allowance[_who] -= _amount;
     }
 }
