2fb014e9cd68caebb9d5d47acd012262ab3276ec
Rafael Flores <fita500@gmail.com>
2021-09-22 16:10:17 -0600

    Improve approach 1

--- supply-chain/ItemManager.sol
+++ supply-chain/ItemManager.sol
@@ -1,5 +1,34 @@
 pragma solidity ^0.6.0;
 
+contract Item {
+    uint256 public priceInWei;
+    uint256 public pricePaid;
+    uint256 public index;
+
+    ItemManager parentContract;
+
+    constructor(
+        ItemManager _parentContract,
+        uint256 _priceInWei,
+        uint256 _index
+    ) public {
+        priceInWei = _priceInWei;
+        index = _index;
+        parentContract = _parentContract;
+    }
+
+    receive() external payable {
+        require(pricePaid == 0, "Item is already paid.");
+        require(priceInWei == msg.value, "Only full payment allowed.");
+        (bool seccess, ) = address(parentContract).call.value(msg.value)(
+            abi.encodeWithSignature("triggerPayment(uint256)", index)
+        );
+        require(success, "The transaction wasn't successful, canceling.");
+    }
+
+    fallback() external {}
+}
+
 contract ItemManager {
     enum SupplyChainState {
         Created,
@@ -8,6 +37,7 @@ contract ItemManager {
     }
 
     struct S_Item {
+        Item _item;
         string _identifier;
         uint256 _itemPrice;
         ItemManager.SupplyChainState _state;
@@ -16,13 +46,23 @@ contract ItemManager {
     mapping(uint256 => S_Item) public items;
     uint256 itemIndex;
 
-    event SupplyChainStep(uint256 _itemIndex, uint256 _step);
+    event SupplyChainStep(
+        uint256 _itemIndex,
+        uint256 _step,
+        address _itemAddress
+    );
 
     function createItem(string memory _identifier, uint256 _itemPrice) public {
+        Item item = new Item(this, _itemPrice, itemIndex);
+        items[itemIndex]._item = item;
         items[itemIndex]._identifier = _identifier;
         items[itemIndex]._itemPrice = _itemPrice;
         items[itemIndex]._state = SupplyChainState.Created;
-        emit SupplyChainStep(itemIndex, uint256(items[itemIndex]._state));
+        emit SupplyChainStep(
+            itemIndex,
+            uint256(items[itemIndex]._state),
+            address(item)
+        );
         itemIndex++;
     }
 
@@ -37,7 +77,11 @@ contract ItemManager {
         );
         items[_itemIndex]._state = SupplyChainState.Paid;
 
-        emit SupplyChainStep(_itemIndex, uint256(items[itemIndex]._state));
+        emit SupplyChainStep(
+            _itemIndex,
+            uint256(items[itemIndex]._state),
+            address(items[_itemIndex]._item)
+        );
     }
 
     function triggerDelivery(uint256 _itemIndex) public {
@@ -46,6 +90,10 @@ contract ItemManager {
             "Item is further in the chain."
         );
         items[_itemIndex].state = SupplyChainState.Delivered;
-        emit SupplyChainStep(_itemIndex, uint256(items[itemIndex]._state));
+        emit SupplyChainStep(
+            _itemIndex,
+            uint256(items[itemIndex]._state),
+            address(items[_itemIndex]._item)
+        );
     }
 }
